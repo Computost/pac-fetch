6bb3629132a0c30e28ce49f4e39a8cbfadc75450
Melody Universe <melody.universe.2021@gmail.com>
2023-04-02 19:06:46 -0700

    more coverage

--- src/fetchPowerPlatformCli.test.ts
+++ src/fetchPowerPlatformCli.test.ts
@@ -6,62 +6,81 @@ import { mockServer } from "./mock/server";
 import { mockDownload, mockPackageMetadata } from "./nuget/mock";
 import specifications from "./specifications";
 import { createZipFile, getDirectoryContents } from "./unzip/mock";
+import inOneLine from "./util/inOneLine";
 
 vi.mock("os");
 vi.mock("url");
 
+beforeEach(() => {
+  vi.useFakeTimers({ toFake: ["Date"] });
+});
+afterEach(() => {
+  vi.useRealTimers();
+});
+
 describe("fetchPowerPlatformCli", () => {
-  beforeEach(() => {
-    vi.useFakeTimers({ toFake: ["Date"] });
-  });
-  afterEach(() => {
-    vi.useRealTimers();
+  it('throws an error when "all" and "operatingSystem" are both specified', async () => {
+    await expect(
+      fetchPowerPlatformCli({ all: true, operatingSystem: "linux" })
+    ).rejects.toThrowError(
+      'Conflicting options: cannot specify both "operatingSystem" and "all."'
+    );
   });
 
   specifications.forEach((spec) => {
-    it(`Default options (${spec.os})`, async () => {
-      const toolsDirectory = {
-        "Azure.Core.dll": "Azure Core assembly",
-        [`pac${spec.os === "windows" ? ".exe" : ""}`]: "Pac executable",
-      };
-      const nugetPackage = await createZipFile({
-        LICENSE: "This is a license file",
-        tools: toolsDirectory,
-      });
-      mockServer(
-        mockPackageMetadata(spec.id, {
-          items: [
-            { items: [{ catalogEntry: { version: "1.0.0" } }], upper: "1.0.0" },
-          ],
-        }),
-        mockDownload(spec.id, "1.0.0", nugetPackage)
-      );
-      (platform as Mock).mockReturnValue(spec.platform);
-      (fileURLToPath as Mock).mockReturnValue(
-        "/home/user/.npm/_npx/hash/node_modules/.bin/pac-fetch/src/fetchPowerPlatformCli.ts"
-      );
-      const now = new Date(2000, 0, 1);
-      vi.setSystemTime(now);
+    describe(`operating system: ${spec.os}`, () => {
+      it(
+        inOneLine`
+          when run for the first time with no parameters,
+            saves pac tools to "bin" folder in package directory`,
+        async () => {
+          const toolsDirectory = {
+            "Azure.Core.dll": "Azure Core assembly",
+            [`pac${spec.os === "windows" ? ".exe" : ""}`]: "Pac executable",
+          };
+          const nugetPackage = await createZipFile({
+            LICENSE: "This is a license file",
+            tools: toolsDirectory,
+          });
+          mockServer(
+            mockPackageMetadata(spec.id, {
+              items: [
+                {
+                  items: [{ catalogEntry: { version: "1.0.0" } }],
+                  upper: "1.0.0",
+                },
+              ],
+            }),
+            mockDownload(spec.id, "1.0.0", nugetPackage)
+          );
+          (platform as Mock).mockReturnValue(spec.platform);
+          (fileURLToPath as Mock).mockReturnValue(
+            "/home/user/.npm/_npx/hash/node_modules/.bin/pac-fetch/src/fetchPowerPlatformCli.ts"
+          );
+          const now = new Date(2000, 0, 1);
+          vi.setSystemTime(now);
 
-      const pacPath = await fetchPowerPlatformCli();
+          const pacPath = await fetchPowerPlatformCli();
 
-      expect(pacPath).toEqual(
-        "/home/user/.npm/_npx/hash/node_modules/.bin/pac-fetch/bin"
-      );
-      const directoryContents = await getDirectoryContents(
-        "/home/user/.npm/_npx/hash/node_modules/.bin/pac-fetch/bin"
-      );
+          expect(pacPath).toEqual(
+            "/home/user/.npm/_npx/hash/node_modules/.bin/pac-fetch/bin"
+          );
+          const directoryContents = await getDirectoryContents(
+            "/home/user/.npm/_npx/hash/node_modules/.bin/pac-fetch/bin"
+          );
 
-      const pacFetchConfig = JSON.parse(
-        directoryContents["pac-fetch.json"] as string
+          const pacFetchConfig = JSON.parse(
+            directoryContents["pac-fetch.json"] as string
+          );
+          delete directoryContents["pac-fetch.json"];
+          expect(directoryContents).toEqual(toolsDirectory);
+          expect(pacFetchConfig).toEqual({
+            expiry: now.getTime() + 60 * 60 * 1000,
+            operatingSystems: { [spec.os]: "1.0.0" },
+            version: "latest",
+          });
+        }
       );
-      delete directoryContents["pac-fetch.json"];
-      expect(directoryContents).toEqual(toolsDirectory);
-      expect(pacFetchConfig).toEqual({
-        expiry: now.getTime() + 60 * 60 * 1000,
-        operatingSystems: { [spec.os]: "1.0.0" },
-        version: "latest",
-      });
     });
   });
 });
